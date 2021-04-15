1d2f148821a7dbe00e62ae24682bde3b0c380b8a
Ash <amc@copper.lan>
2021-04-15 12:11:12 +0100

    Better navbar styling

--- src/dcs/prototype_6/ui_nav.cljs
+++ src/dcs/prototype_6/ui_nav.cljs
@@ -19,76 +19,92 @@
 (defn close-burger
       "Close-up the burger's dropdowns and the burger itself"
       [& _]
-      (doseq [id ["explore-checkbox" "datasets-checkbox" "about-checkbox" "toggler" ]]
+      ;;  Comment-out. Isn't affecting clicked-on dropdowns as I'd like. Maybe give-up on this.
+      ;;  (doseq [id ["explore-dropdown" "data-dropdown" "about-dropdown" "toggler" ]]
+      ;;         (.remove (.-classList (.getElementById js/document id)) "is-active"))
+      (doseq [id ["explore-checkbox" "data-checkbox" "about-checkbox" "toggler" ]]
              (set! (.-checked (.getElementById js/document id)) false)))
 
+(defn navbar-clickable
+      "Clickable navbar items should close the burger.
+      This codes that."
+      [href]
+      {:href href
+       :on-click close-burger})
 
 (defn navbar []
       [:nav.navbar.is-fixed-top.is-primary {:role "navigation"}
+
        [:input#toggler.toggler {:type "checkbox"}]
        [:div.navbar-brand
         [:a.navbar-item {:href "#"}
          [:img.brand-logo {:src "img/dcs-circle.png" :alt "Waste Matters Scotland logo"}]
          "Waste Matters Scotland"]
         [:a.navbar-item]
+
         [:label.navbar-burger.burger {:data-target "topnavbar" :for "toggler" :role "button"}
          [:span]
          [:span]
          [:span]]]
+
        [:div#topnavbar.navbar-menu
+
         [:div.navbar-start
          #_[:a.navbar-item {:href "#"} "Left side items beside the brand logo"]]
+
         [:div.navbar-end
+
+         ;; Explore
          [:div.navbar-item.has-dropdown.is-hoverable.is-right
           [:input#explore-checkbox {:type "checkbox"}]
           [:label.navbar-link {:for "explore-checkbox"} "Explore"]
-          [:div#explore-dropdown.navbar-dropdown
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/dashboard-page) :on-click close-burger}
+          [:div#explore-dropdown.navbar-dropdown.is-right
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/dashboard-page))
             [:div.navbar-content
-             [:p [:strong "Waste by region"]]
-             [:p [:small "Discover and compare regional waste figures"]]]]
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/todo-page) :on-click close-burger}
+             "Waste by region"
+             [:p.is-size-7.has-text-info "Discover and compare regional waste figures"]]]
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/todo-page))
             [:div.navbar-content
-             [:p [:strong "Waste through time"]]
-             [:p [:small "Discover and compare waste figures through time"]]]]
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/todo-page) :on-click close-burger}
+             "Waste through time"
+             [:p.is-size-7.has-text-info "Discover and compare waste figures through time"]]]
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/todo-page))
             [:div.navbar-content
-             [:p [:strong "Dimensions overview"]]
-             [:p [:small "The list of our datasets' dimensions"]]]]]]
+             "Dimensions overview"
+             [:p.is-size-7.has-text-info "The list of our datasets' dimensions"]]]]]
+
+         ;; Data
          [:div.navbar-item.has-dropdown.is-hoverable
-          [:input#datasets-checkbox {:type "checkbox"}]
-          [:label.navbar-link {:for "datasets-checkbox"} "Datasets"]
-          [:div#datasets-dropdown.navbar-dropdown
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/todo-page) :on-click close-burger}
+          [:input#data-checkbox {:type "checkbox"}]
+          [:label.navbar-link {:for "datasets-checkbox"} "Data"]
+          [:div#data-dropdown.navbar-dropdown.is-right
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/todo-page))
             [:div.navbar-content
-             [:p [:strong "Overview"]]
-             [:p [:small "A description of our datasets"]]]]
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/todo-page) :on-click close-burger}
+             "Overview"
+             [:p.is-size-7.has-text-info "A description of our datasets"]]]
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/todo-page))
             [:div.navbar-content
-             [:p [:strong "Dimensions overview"]]
-             [:p [:small "The list of our datasets' dimensions"]]]]
+             "Dimensions overview"
+             [:p.is-size-7.has-text-info "The list of our datasets' dimensions"]]]
            [:br.navbar-divider]
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/todo-page) :on-click close-burger}
+           [:div.navbar-item
+            [:p.has-text-info "Directly download our datasets"]]
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/todo-page))
             [:div.navbar-content
-             [:p [:strong "CO2e"]]
-             [:p [:small "What does this look like?"]]]]
-           [:br.navbar-divider]
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/route1-page) :on-click close-burger}
+             "CO2e"]]
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/route1-page) )
             [:div.navbar-content
-             [:p
-              "Route 1"]
-             [:p "Take route 1"]]]]]
+             "Route 1"]]]]
+
+         ;; About
          [:div.navbar-item.has-dropdown.is-hoverable
           [:input#about-checkbox {:type "checkbox"}]
           [:label.navbar-link {:for "about-checkbox"} "About"]
-          [:div#about-dropdown.navbar-dropdown
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/home-page) :on-click close-burger}
+          [:div#about-dropdown.navbar-dropdown.is-right
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/home-page))
             [:div.navbar-content
-             [:p [:strong "Home page"]]
-             [:p [:small "Go to the splash page"]]]]
-           [:br.navbar-divider]
-           [:a.navbar-item {:href (rfe/href :dcs.prototype-6.browser/route1-page) :on-click close-burger}
+             "Home page"
+             [:p.is-size-7.has-text-info"Go to the splash page"]]]
+           [:a.navbar-item (navbar-clickable (rfe/href :dcs.prototype-6.browser/route1-page) )
             [:div.navbar-content
-             [:p
-              "Route 1"]
-             [:p "Take route 1"]]]]]]]])
+             "Route 1"
+             [:p.is-size-7.has-text-info "Take route 1"]]]]]]]])
