d27f483584009ff102beab0162bafc1f8db0f53a
Ash <amc@copper.lan>
2021-04-26 12:09:19 +0100

    Use 'message' styled boxes to yeild a more ONS like page for Stir bins

--- src/dcs/prototype_6/view/stirling_bin_collection/main.cljs
+++ src/dcs/prototype_6/view/stirling_bin_collection/main.cljs
@@ -13,14 +13,14 @@
 
        [:div.container
         [:div.content
-         [:p "Stirling Council has set a precedent by being the first (and " [:em "still"] " only) Scottish local authority
+         [:p "Stirling Council set a precedent by being the first (and " [:em "still"] " only) Scottish local authority
         to have published " [:b "open data"] " about their bin collection of household waste.
         This data is contained in the "
           [:a {:href "https://data-commons-scotland.github.io/dcs-wcs-prototype-3/pages-output/data/about/index.html#stirling-bin-collection"}
            "stirling-bin-collection"] " dataset."]
-         [:p "The council are currently working on increasing the fidelity of this dataset
-        (e.g. by adding spatial data to describe collection routes)
-        however, we can still squeeze from it, several interesting bits of information..."]
+         [:p "The council are currently working on increasing the fidelity of this dataset,
+        e.g. by adding spatial data to describe collection routes.
+        However, we can still squeeze from its current version, several interesting pieces of information..."]
          [:br]]]
 
        [:div.tile.is-ancestor
@@ -28,65 +28,77 @@
         [:div.tile.is-6
          [:div.tile.is-vertical.is-parent
 
-          [:div.tile.is-child.box
-           [:figure
-            [derivation-generation/root]]
-           [:h2.title.is-6.has-text-danger "Amount"]
-           [:h2.subtitle.is-5 "How much material was in the bins?"]
-           [:div.content.has-text-success
-            [:p "The amount of household waste per citizen, accounted for through bin collection,
+          [:div.tile.is-child.message.has-background-white.is-info
+           [:div.message-header
+            [:div
+             [:h2.title.is-6.has-text-weight-bold "Amount"]
+             [:h2.subtitle.is-5.has-text-primary-light "How much material was in the bins?"]]]
+           [:div.message-body
+            [:figure
+             [derivation-generation/root]]
+            [:div.content.has-text-warning-dark.has-text-weight-bold
+             [:p "The amount of household waste per citizen, accounted for through bin collection,
            has slightly increased."]]
-           [:p "To provide context, the graph depicts the amounts accounted
+            [:p "To provide context, the graph depicts the amounts accounted
            for through " [:em "all means"] " for Stirling and Scotland.
            Perhaps the approximately 0.1 tonne gap is due to the household waste
            that deposited at tip sites by citizens themselves(?)."]
-           [:br]
-           [:div.content.is-small.has-text-info
-            [:p "The bin collection data for 2021 contains only Q1 data,
+            [:br]
+            [:div.content.is-small.has-text-info
+             [:p "The bin collection data for 2021 contains only Q1 data,
            so we've removed the 2021 data from this year-on-year comparison.
            The population data for 2020 and 2021 was not available to us,
-           so we're using 2019 population figures for those years."]]]
+           so we're using 2019 population figures for those years."]]]]
 
-          [:div.tile.is-child.box
-           [:figure
-            [derivation-composition/root]]
-           [:h2.title.is-6.has-text-danger "Composition"]
-           [:h2.subtitle.is-5 "What types of material were in the bins?"]
-           [:div.content.has-text-warning
-            [:p [:em "Unsorted"] " waste consistently has been the biggest component of bin-collected waste."]]
-           [:div.content.is-small.has-text-info
-            [:p "These figures are based on a coarse-grained mapping from the values
-            supplied in Stirling council's bin collection dataset to SEPA's "
-            [:a {:href "https://data-commons-scotland.github.io/dcs-wcs-prototype-3/pages-output/data/about/index.html#material-coding"} "material-coding"]
-             ". The data for 2021 contains only Q1 data, hence 2021's lower amounts."]]]]]
-
-        [:div.tile.is-6
-         [:div.tile.is-vertical.is-parent
-
-          [:div.tile.is-child.box
-           [:figure
-            [derivation-percent-recycled/root]]
-           [:h2.title.is-6.has-text-danger "Recycling"]
-           [:h2.subtitle.is-5 "What percentage was placed into the recycling collection?"]
-           [:div.content.has-text-success
-            [:p "The percentage of household waste, accounted for through the recycling bin collection,
+          [:div.tile.is-child.message.has-background-white.is-link
+           [:div.message-header
+            [:div
+             [:h2.title.is-6.has-text-weight-bold "Recycling"]
+             [:h2.subtitle.is-5.has-text-primary-light "What percentage was put into the recycling collection?"]]]
+           [:div.message-body
+            [:figure
+             [derivation-percent-recycled/root]]
+            [:div.content.has-text-success-dark.has-text-weight-bold
+             [:p "The percentage of household waste, accounted for through the recycling bin collection,
            has increased."]]
-           [:p "To provide context, the graph depicts the " [:em "overall"] " percentages
+            [:p "To provide context, the graph depicts the " [:em "overall"] " percentages
            of household waste recycled in Stirling and Scotland.
            Perhaps the significant gap is due to
            the " [:em "waste sorting for recycling"] " that occurs downstream from bin collection
            and at tip sites."]
-           [:br]
-           [:div.content.is-small.has-text-info
-            [:p "The bin collection data for 2021 contains only Q1 data,
-            so this might not be a good representation of the whole of the year 2021."]]]
+            [:br]
+            [:div.content.is-small.has-text-info
+             [:p "The bin collection data for 2021 contains only Q1 data,
+            so this might not be a good representation of the whole of the year 2021."]]]]]]
 
-          [:div.tile.is-child.box
-           [:figure
-            [derivation-missed/root]]
-           [:h2.title.is-6.has-text-danger "Missed bins"]
-           [:h2.subtitle.is-5 "How many bins were missed?"]
-           [:div.content.has-text-success
-            [:p "The amount of household waste accounted for as "
-             [:em "missed bins"] " fell significantly between Q1 2018 and Q3 2019."]]]]]]])
+        [:div.tile.is-6
+         [:div.tile.is-vertical.is-parent
+
+          [:div.tile.is-child.message.has-background-white.is-info
+           [:div.message-header
+            [:div
+             [:h2.title.is-6.has-text-weight-bold "Composition"]
+             [:h2.subtitle.is-5.has-text-primary-light "What types of material were in the bins?"]]]
+           [:div.message-body
+            [:figure
+             [derivation-composition/root]]
+            [:div.content.has-text-warning-dark.has-text-weight-bold
+             [:p [:em "Unsorted"] " waste consistently has been the biggest component of bin-collected waste."]]
+            [:div.content.is-small.has-text-info
+             [:p "These figures are based on a coarse-grained mapping from the values
+            supplied in Stirling council's bin collection dataset to SEPA's "
+              [:a {:href "https://data-commons-scotland.github.io/dcs-wcs-prototype-3/pages-output/data/about/index.html#material-coding"} "material-coding"]
+              ". The data for 2021 contains only Q1 data, hence 2021's lower amounts."]]]]
+
+          [:div.tile.is-child.message.has-background-white.is-link
+           [:div.message-header
+            [:div
+             [:h2.title.is-6.has-text-weight-bold "Missed bins"]
+             [:h2.subtitle.is-5.has-text-primary-light "How many bins were missed?"]]]
+           [:div.message-body
+            [:figure
+             [derivation-missed/root]]
+            [:div.content.has-text-success-dark.has-text-weight-bold
+             [:p "The amount of household waste accounted for as "
+              [:em "missed bins"] " fell significantly between Q1 2018 and Q3 2019."]]]]]]]])
 
