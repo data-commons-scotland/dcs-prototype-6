6c3173c32eced5560aaa89f4734a22342b8e7d88
Ash <amc@zinc.lan>
2021-08-10 08:32:51 +0100

    Added to the home page

--- src/dcs/prototype_6/view/home.cljs
+++ src/dcs/prototype_6/view/home.cljs
@@ -8,7 +8,7 @@
         [:h1.subtitle.is-italic.is-3 {:class colour} title]
         [:p subtitle]]])
 
-(defn leaf-tile [title subtitle body href img-src]
+(defn leaf-tile [title title-colour  subtitle body href img-src]
       [:div.tile.is-child
        [:a {:href href }
        [:div.card.is-shadowless {:style {:backgroundColor "#fff1e5"}}
@@ -17,7 +17,7 @@
           [:img {:src img-src :alt title}]]]
         [:div.card-content.pt-2.pr-1.pl-1
          [:div
-          #_[:h2.title.is-6.has-text-weight-bold.has-text-danger title]
+          [:h2.title.is-7.has-text-weight-bold {:style {:color title-colour}} title]
           [:h2.subtitle.is-5.has-text-danger subtitle]]
          [:p.content.is-small body]
          ]]]])
@@ -50,6 +50,17 @@
           [:p.content.is-small.has-text-primary-light body]
           ]]]])
 
+(defn leaf-tile-blank []
+  [:div.tile.is-child
+   
+    [:div.card.is-shadowless {:style {:backgroundColor "#fff1e5"}}
+     [:div.card-image
+      ]
+     [:div.card-content.pt-2.pr-1.pl-1
+      [:div
+       ]
+      [:p.content.is-small ""]]]])
+
 (defn root []
       [:div
 
@@ -66,44 +77,65 @@
           [:div.tile.is-ancestor
 
            [:div.tile.is-vertical.is-parent.is-3
-            (leaf-tile "Article"
+            (leaf-tile "Government data"
+                       "#1f77b4"
                        "Household waste analysis"
                        "What do households put into their bins and how appropriate are their disposal decisions?"
                        (rfe/href :dcs.prototype-6.router/household-waste-analysis-view)
                        "img/analysing-waste-composition.jpg")
 
-            (leaf-tile "Tool"
+            (leaf-tile "Government data"
+                       "#1f77b4"
                        "Waste by region"
                        "How is waste in your region? Explore and compare regional waste amounts."
                        (rfe/href :dcs.prototype-6.router/dashboard-view)
-                       "img/regional-dashboard.png")]
+                       "img/regional-dashboard.png")
+            
+            (leaf-tile "Local reuse organisation data"
+                       "#fdae6b"
+                       "Stirling Community Food"
+                       "Discover, through data, how this project is reducing a community's wasted food."
+                       (rfe/href :dcs.prototype-6.router/stirling-community-food-view)
+                       "img/stirling-community-food.png")]
 
            [:div.tile.is-vertical.is-parent.is-3
-            (leaf-tile "Tool"
+            (leaf-tile "Government data"
+                       "#1f77b4"
                        "Waste sites"
                        "Explore the location of waste sites and quantities of materials that they receive."
                        (rfe/href :dcs.prototype-6.router/waste-sites-map-view)
                        "img/waste-site.jpeg")
+            
+            (leaf-tile "Local reuse organisation data"
+                       "#fdae6b"
+                       "ACE Furniture"
+                       [:span "Find out about the amounts and types of furniture that are being reused because of this initiative."]
+                       (rfe/href :dcs.prototype-6.router/ace-furniture-view)
+                       "img/ace-furniture.png")
 
-            (leaf-tile "Article"
+            (leaf-tile "Government data"
+                       "#1f77b4"
                        "Stirling's bin collection"
                        "Interesting facts about Stirling's bin collection of household waste."
                        (rfe/href :dcs.prototype-6.router/stirling-bin-collection-view)
                        "img/looking-at-bin-collections.jpeg")]
 
            [:div.tile.is-vertical.is-parent.is-3
-            (leaf-tile "Article"
-                       "Stirling Community Food"
-                       "Discover, through data, how this project is reducing a community's wasted food."
-                       (rfe/href :dcs.prototype-6.router/stirling-community-food-view)
-                       "img/avoid-wasting-food.png")
+            (leaf-tile "Local reuse organisation data"
+                       "#fdae6b"
+                       "The Fair Share"
+                       [:span "Discover how many cars worth of CO" [:sub "2"] "e is avoided each year because of this university based, reuse store."]
+                       (rfe/href :dcs.prototype-6.router/fairshare-view)
+                       "img/fairshare.png")
 
-
-            (leaf-tile "Tool"
+            (leaf-tile "Government data"
+                       "#1f77b4"
                        "Waste through the decade"
                        [:span "Discover how waste, recycling and CO" [:sub "2"] "e amounts have been changing across Scotland over the last 10 years"]
                        (rfe/href :dcs.prototype-6.router/waste-through-time-map-view {:preset "hw-mgmt"})
                        "img/map-mgmt.gif")
+            
+            (leaf-tile-blank)
             ]
 
            [:div.tile.is-vertical.is-parent.is-3
@@ -117,7 +149,9 @@
                        "This site and the project"
                        "Find out about the ideas behind this website and about its encompassing project, Data Commons Scotland."
                        (rfe/href :dcs.prototype-6.router/about-view)
-                       "img/project-team.png")]
+                       "img/project-team.png")
+            
+            (leaf-tile-blank)]
            ]
           ]
          ]
