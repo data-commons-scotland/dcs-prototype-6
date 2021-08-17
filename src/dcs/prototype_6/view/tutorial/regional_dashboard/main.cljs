642fa700e71a9341739d1f87770291d77b467820
anwstirling <88778743+anwstirling@users.noreply.github.com>
2021-08-17 16:02:06 +0100

    Update main.cljs

--- src/dcs/prototype_6/view/tutorial/regional_dashboard/main.cljs
+++ src/dcs/prototype_6/view/tutorial/regional_dashboard/main.cljs
@@ -25,7 +25,7 @@
       [:li "understand what the data are and where they come from"]
       [:li "interpret the visualisations and ask questions about the data and what they mean"]]
      
-     "What are the data and where do they come from?"
+     "[:h2 "What are the data and where do they come from?"]"
      
      [:p "The " [:em "Regional Dashboard"] " pages display a range of data sets relating to household"
       " and business waste for each of Scotland's unitary authorities."]
@@ -39,7 +39,7 @@
       " waste generated " [:em "per person"] " and not to total amounts of waste. If you scroll down, you will also be able to see"
       " data relating to business waste."]
      
-     "Interpreting the graphs and other data visualisations"
+     "[:h2 "Interpreting the graphs and other data visualisations"]"
      
      [:p "The first six data visulaisations displayed on these pages relate to " [:em "household waste"] " ."]
      [:p " " [:em "Ranking"] " information that tells you how this region compares to other regions"
@@ -90,6 +90,47 @@
        [hw-gen/chart
         "Stirling"
         household-waste-derivation-generation]]]]
+     
+      [:p "Once you have satisfied yourself that any variations between years are significant, it's time to start asking questions" 
+        " about why they might have happened. It may be that there was a change in how something was reported, or in a region's waste"
+        " collections or waste management contracts.  Or it may be that there was a change in the actual waste being generated. "
+        " The next two graphs you see may contain clues to help us understand what is going on. The first of these is the"
+        " "[:em "Composition"]" graph."]
+     
+     [:div.columns.is-vcentered
+      
+      [:div.column.is-one-quarter
+       [:div.content
+        "Insert Stirling Composition graph here"
+        ]]
+      [:div.column
+       [:div.content
+        [:p "The "[:em "Composition"]" graph shows us information about the types of waste that make up the household waste stream."
+         " The graph takes the form of a stacked bar chart, where the different colours in a single column indicate different categories"
+         " of waste.  These are the categroies used by SEPA and by local authorities when they report to SEPA.  If you move your mouse"
+         " up and down a column, you will see what the different waste categories are.  Stirloing's figures tell us that the dominant"
+         " component of household waste falls into the category "[:em "Household and similar wastes"]", which is probably not surprising."
+         " The second biggest contribution to household waste falls into the category "[:em "Vegetal wastes"]". It's worth asking just"
+         " what this category means and whether it is the same for all regions.  For the period covered by the graph, Stirling operated"
+         " separate collections for food and garden wastes.  It may be that its garden waste collections were reported as "[:em "Vegetal"]""
+         " while domestic food waste may have been reported under "[:em "Animal and mixed food wastes"]"."]]]]
+     
+     [:div.columns.is-vcentered
+   
+      [:div.column
+       [:div.content
+        [:p "Next to the "[:em "Composition"]" graph, you can see the "[:em "Management"]" graph, which shows us information about where"
+         " our household wastes end up. This is displayed as another stacked bar chart, with just three possible destinations: "[:em "Landfilled"]","
+         " "[:em "Recycled"]" and "[:em "Other diversion"]". This last category can include more environmentally sustainable destinations such"
+         " as biomass energy generation, but it can also include incineration.  The data show that Stirling doesn't send a great deal of its household" 
+         " waste to destinations other than landfill or recycling, but we can see that in some years there has been a more singificant amount of"
+         " "[:em "Other Diversion"]" than others.  You might want to ask your local authority what this category means for them - and if you live"
+         " in Stirling, you might want to ask why there was more of this reported in 2016 than in any other year."]]]
+      
+      [:div.column.is-one-quarter
+       [:div.content
+        "Insert Stirling Management graph here"
+        ]]]
      
      ]]])
 
